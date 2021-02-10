93def925fd5f4bdf0bd9253b71d4780433e1666d
Kiara McNulty <kiaramcnulty@gmail.com>
2021-02-10 16:07:53 -0600

    finished award presenters

--- gg-project-master-master/README.md
+++ gg-project-master-master/README.md
@@ -1,2 +1,28 @@
-# gg-project-master
-Golden Globe Project Master
+# CS337-Project-1
+
+Minimum Requirements:
+
+Fulfilling only the minimum requirements puts your group on track for a B
+
+A project must do a reasonable job identifying each of these components.
+
+- Host(s) (for the entire ceremony)
+- Award Names
+- Presenters, mapped to awards*
+- Nominees, mapped to awards*
+- Winners, mapped to awards*
+*These will default to using a hardcoded list of the awards to avoid penalizing you for cascading error. Please note that, when mining award names specifically, you cannot hardcode parts of these names in your solution.
+
+It is OK not to have 100% accuracy. It's very rare for any group not to have some error, especially with nominees. Even getting just half of the nominees for a given award is quite good performance.
+
+Additional Goals:
+
+To get better than a B, you must do exceptionally well on the minimum requirements, or complete one or more additional goals. Some examples of additional goals:
+
+- Red carpet – For example, determine who was best dressed, worst dressed, most discussed, most controversial, or perhaps find pictures of the best and worst dressed, etc.
+- Humor – For example, what were the best jokes of the night, and who said them?
+- Parties – For example, what parties were people talking about the most? Were people saying good things, or bad things?
+- Sentiment – What were the most common sentiments used with respect to the winners, hosts, presenters, acts, and/or nominees?
+- Acts – What were the acts, when did they happen, and/or what did people have to say about them?
+- Your choice – If you have a cool idea, suggest it to the TA! Ideas that will require the application of NLP and semantic information are more likely to be approved.
+Typical performance on the minimum requirements, plus a well-done additional goal, is likely to earn an A- or better.
--- gg-project-master-master/gg_api.py
+++ gg-project-master-master/gg_api.py
@@ -3,10 +3,84 @@
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
 
+import numpy as np
+import pandas as pd
+import spacy
+from spacy import displacy
+from collections import defaultdict
+import Levenshtein
+import operator
+
+from spacy.matcher import Matcher
+from spacy.tokens import Span
+from string import punctuation
+
+nlp = spacy.load('en_core_web_md')
+matcher = Matcher(nlp.vocab)
+
+if year == '2013' or year == '2015':
+    new_award_names = OFFICIAL_AWARDS_1315
+else:
+    new_award_names = OFFICIAL_AWARDS_1819
+
+def DistMeasure(tweet):
+    awards = {}
+    tweet = tweet.lower()
+    tweet = tweet.replace('tv', 'television')
+    tweet = tweet.replace('film', 'motion picture')
+    tweet = tweet.replace('movie', 'motion picture')
+    for award in new_award_names:
+        split_award = award.split()
+        award_dist = 0
+        
+        split_tweet = tweet.split()
+        for word in split_tweet:
+            array = []
+            for word2 in split_award:
+                array.append(Levenshtein.distance(word, word2))
+            min_val = array[np.argmin(array)]
+            award_dist += min_val
+        awards[award] = award_dist
+        
+    val = min(awards.values())
+    res = [key for key in awards if awards[key] == val]
+    if len(res) > 10:
+        res = []
+    return res
+
 def get_hosts(year):
     '''Hosts is a list of one or more strings. Do NOT change the name
     of this function or what it returns.'''
     # Your code here
+    df = pd.read_json(('../gg' + year + '.json'))
+    df = df['text']
+    host_df = df[df.str.contains('host|hosts|hostess|hosted|hosting', case = False)]
+    df = df[~df.str.contains('think|thinking|should|maybe', case = False)]
+    df = df.str.replace('#GoldenGlobes|golden|globes|globe', "", case = False)
+    if host_df.size > 3000:
+        host_df = host_df.sample(3000)
+
+    hosts = {}
+    hosts = defaultdict(lambda: 1, hosts)
+    for i, value in host_df.iteritems():
+        for entity in nlp(value).ents:
+            if entity.label_ == 'PERSON':
+                hosts[entity.text] = hosts[entity.text] +1
+                
+    hosts = sorted(hosts.items(), key=lambda item: item[1], reverse = True)
+
+    top30 = hosts[:30]
+    for name in range(5):
+        first_name = top30[name][0].split()[0]
+        for other in range(29-name):
+            curr_name = top30[other+name+1][0]
+            if first_name in curr_name:
+                count = top30[name][1] + top30[other+name+1][1]
+                top30[name] = (top30[name][0], count)
+
+    final30 = sorted(top30, key=lambda item: item[1], reverse = True)
+    hosts = (final30[0][0], final30[1][0])
+
     return hosts
 
 def get_awards(year):
@@ -27,6 +101,80 @@ def get_winner(year):
     names as keys, and each entry containing a single string.
     Do NOT change the name of this function or what it returns.'''
     # Your code here
+    df = pd.read_json('../gg' + year + '.json')
+    df = df['text']
+    df = df[df.str.contains('won best|wins best|goes to', case = False)]
+    df = df[~df.str.contains('think|thinking|should|maybe', case = False)]
+    wins_df = df.str.replace('#GoldenGlobes|golden|globes|globe', "", case = False)
+    if wins_df.size > 5000:
+        wins_df = wins_df.sample(5000)
+
+    pattern1 = [{"ENT_TYPE": "PERSON", 'OP':'+'}, {"LEMMA": "win"}, {"ORTH":'Best'}, {"DEP": 'compound', 'OP':'+'}, {"POS":'NOUN', 'OP':'*'}, {'IS_PUNCT': True, 'OP':'?'}, {"POS": 'PROPN', 'OP':'*'}]
+    pattern2 = [{"ENT_TYPE": "WORK_OF_ART", 'OP':'+'}, {"LEMMA": "win"}, {"ORTH":'Best'}, {"DEP": 'compound', 'OP':'+'}, {"POS":'NOUN', 'OP':'*'}, {'IS_PUNCT': True, 'OP':'?'}, {"POS": 'PROPN', 'OP':'*'}]
+    pattern3 = [{"ENT_TYPE": "PERSON", 'OP':'+'}, {"LEMMA": "win"}, {"ORTH":'Best'}, {"ENT_TYPE": "WORK_OF_ART", 'OP':'+'}]
+    pattern4 = [{"ENT_TYPE": "WORK_OF_ART", 'OP':'+'}, {"LEMMA": "win"}, {"ORTH":'Best'}, {"ENT_TYPE": "WORK_OF_ART", 'OP':'+'}]
+    pattern5 = [{"ORTH":'Best'}, {"ENT_TYPE": "WORK_OF_ART", 'OP':'+'},{"TEXT": "goes"}, {"TEXT": "to"}, {"ENT_TYPE": "PERSON", 'OP':'+'}] 
+    pattern6 = [{"ORTH":'Best'}, {"DEP": 'compound', 'OP':'+'}, {"POS":'NOUN', 'OP':'*'}, {'IS_PUNCT': True, 'OP':'?'}, {"POS": 'PROPN', 'OP':'*'}, {"TEXT": "goes"}, {"TEXT": "to"}, {"ENT_TYPE": "PERSON", 'OP':'+'}] 
+    
+    matcher.add("Winner", [pattern1, pattern2, pattern3, pattern4, pattern5, pattern6])
+
+    winners = {}
+    winners = defaultdict(lambda: 1, winners)
+    for i, text in wins_df.iteritems():
+        doc = nlp(text)
+        matches = matcher(doc)
+        if len(matches) != 0:
+            span = doc[matches[-1][1]:matches[-1][2]]
+            winners[str(span).strip(punctuation).strip()] = winners[str(span).strip(punctuation).strip()] +1
+    
+    winners = sorted(winners.items(), key=lambda item: item[1], reverse = True)
+
+    award_list = [dict() for x in range(len(new_award_names))]
+
+    for tweet in winners: 
+        tweet = tweet[0]
+        split = tweet.lower().split()
+        
+        index = split.index("best")
+
+        award_phrase = " ".join(split[index:])
+        matching_awards = DistMeasure(award_phrase)
+        for a in matching_awards:
+            award_index = new_award_names.index(a)
+            global person
+            person = ''
+            if "goes" in split and "to" in split:
+                print("goes")
+                person_index = split.index("goes")
+                person = split[person_index+2:]
+                print(person)
+            else:
+                person = split[:index-1]
+            person = " ".join(person)
+            if person not in award_list[award_index]:
+                award_list[award_index][person] = 1
+            else:
+                award_list[award_index][person] = award_list[award_index][person]+1
+
+    i = 0
+    awards = [[] for x in range(len(new_award_names))]
+    for award in award_list:
+        if award != {}:
+            max_val = max(award.items(), key=operator.itemgetter(1))[1]
+            j = 0
+            list_award = list(award)
+            for b in award:
+                if award[b] == max_val and len(awards[i]) < 1:
+                    awards[i].append(list_award[j])
+                j+=1
+        i+=1
+
+    for award in awards: 
+    if len(award) == 0:
+        award.append("NO WINNER FOUND")
+    award_flat = np.array(awards).flatten()
+    winners = dict(zip(award_names, award_flat)) 
+
     return winners
 
 def get_presenters(year):
@@ -34,7 +182,68 @@ def get_presenters(year):
     names as keys, and each entry a list of strings. Do NOT change the
     name of this function or what it returns.'''
     # Your code here
+    df = pd.read_json('../gg' + year + '.json')
+    df = df['text']
+    df = df[df.str.contains(' present', case = False)]
+    df = df[~df.str.contains('think|should|maybe', case = False)]
+    present_df = df.str.replace('#GoldenGlobes|golden|globes|globe', "", case = False)
+    if present_df.size > 5000:
+        present_df = present_df.sample(5000)
+        return presenters
+
+    pattern1 = [{"ENT_TYPE": "PERSON", 'OP':'+'}, {"LEMMA": "present"}, {"ORTH":'Best'}, {"DEP": 'compound', 'OP':'+'}, {"POS":'NOUN', 'OP':'*'}, {'IS_PUNCT': True, 'OP':'?'}, {"POS": 'PROPN', 'OP':'*'}]
+    pattern2 = [{"ENT_TYPE": "WORK_OF_ART", 'OP':'+'}, {"LEMMA": "present"}, {"ORTH":'Best'}, {"DEP": 'compound', 'OP':'+'}, {"POS":'NOUN', 'OP':'*'}, {'IS_PUNCT': True, 'OP':'?'}, {"POS": 'PROPN', 'OP':'*'}]
+    pattern3 = [{"ENT_TYPE": "PERSON", 'OP':'+'}, {"LEMMA": "present"}, {"ORTH":'Best'}, {"ENT_TYPE": "WORK_OF_ART", 'OP':'+'}]
+    pattern4 = [{"ENT_TYPE": "WORK_OF_ART", 'OP':'+'}, {"LEMMA": "present"}, {"ORTH":'Best'}, {"ENT_TYPE": "WORK_OF_ART", 'OP':'+'}]
+
+    matcher.add("Presenter", [pattern1, pattern2, pattern3, pattern4])
+
+    presenters = {}
+    presenters = defaultdict(lambda: 1, presenters)
+    for i, text in present_df.iteritems():
+        doc = nlp(text)
+        matches = matcher(doc)
+        if len(matches) != 0:
+            span = doc[matches[-1][1]:matches[-1][2]]
+            presenters[str(span).strip(punctuation).strip()] = presenters[str(span).strip(punctuation).strip()] +1
+
+    presenters = sorted(presenters.items(), key=lambda item: item[1], reverse = True)
+
+    award_list = [dict() for x in range(len(new_award_names))]
+
+    for tweet in presenters: 
+        tweet = tweet[0]
+        split = tweet.lower().split()
+        
+        index = split.index("best")
+
+        award_phrase = " ".join(split[index:])
+        matching_awards = DistMeasure(award_phrase)
+        for a in matching_awards:
+            award_index = new_award_names.index(a)  
+            person = split[:index-1]
+            person = " ".join(person)
+            if person not in award_list[award_index]:
+                award_list[award_index][person] = 1
+            else:
+                award_list[award_index][person] = award_list[award_index][person]+1
+
+    i = 0
+    awards = [[] for x in range(len(new_award_names))]
+    for award in award_list:
+        if award != {}:
+            max_val = max(award.items(), key=operator.itemgetter(1))[1]
+            j = 0
+            list_award = list(award)
+            for b in award:
+                if award[b] == max_val and len(awards[i]) < 2:
+                    awards[i].append(list_award[j])
+                j+=1
+        i+=1
+
+    presenters = dict(zip(award_names, awards)) 
     return presenters
+
 
 def pre_ceremony():
     '''This function loads/fetches/processes any data your program
